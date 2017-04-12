c8f289f2202a0a554a6e5f2a589070f95e916e57
haixiangMAC <HX@HXMac.local>
2017-04-12 15:07:49 +0800

    Initial Commit

---
+++ AddChildVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddChildVC
+//
+//  Created by HXMAC on 2017/4/12.
+//  Copyright © 2017年 HXMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddChildVC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddChildVC
+//
+//  Created by HXMAC on 2017/4/12.
+//  Copyright © 2017年 HXMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
